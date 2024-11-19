190677a2c10630da362f55ed21fb7948051f3358
leonardo-guglielmini <leonardo.guglielmini@outlook.it>
2024-11-19 13:38:41 +0100

    ADD post router content

--- routers/posts.js
+++ routers/posts.js
@@ -3,32 +3,32 @@ const router = express.Router();
 
 //index
 router.get("/", (req, res) => {
-    res.send("");
+    res.send("Lista dei posts");
 })
 
 //show
 router.get("/:id", (req, res) => {
-    res.send("");
+    res.send(`Post: ${req.params.id}`);
 })
 
 //store
 router.post("/", (req, res) => {
-    res.send("");
+    res.send("Inserimento nuovo post");
 })
 
 //update
 router.put("/:id", (req, res) => {
-    res.send("");
+    res.send(`Modifica totale del post: ${req.params.id}`);
 })
 
 //modify
 router.patch("/:id", (req, res) => {
-    res.send("");
+    res.send(`Modifica parziale del post: ${req.params.id}`);
 })
 
 //delete
 router.delete("/:id", (req, res) => {
-    res.send("");
+    res.send(`Cancellazione del post ${req.params.id}`);
 })
 
 module.exports = router;
